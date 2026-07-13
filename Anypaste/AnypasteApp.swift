36510a33476bc4d6d8af4f0e726a7502dcb062e2
Aryan F <aryanfulari@gmail.com>
2026-07-13 13:59:58 +0530

    Initial Commit

---
+++ Anypaste/AnypasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnypasteApp.swift
+//  Anypaste
+//
+//  Created by Aryan Suhas Fulari on 13/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AnypasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
